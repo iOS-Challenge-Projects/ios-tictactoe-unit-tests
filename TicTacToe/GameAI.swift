96d971e7bb6bf9419a73c9035cce8697590155ed
fritzgt <tuanis@live.com>
2020-04-03 15:06:45 +0300

    completed the win method

--- TicTacToe/GameAI.swift
+++ TicTacToe/GameAI.swift
@@ -9,21 +9,64 @@
 import Foundation
 
 func game(board: GameBoard, isWonBy player: GameBoard.Mark) -> Bool {    
+        
+    //checking vertical column
+    for x in 0...2 {
+        var numberOfMarks = 0
+        
+        for y in 0...2{
+            if board[(x,y)] == player{
+                numberOfMarks += 1
+            }
+        }
+        
+        if (numberOfMarks == 3) {
+            return true
+        }
+    }
     
-    //checking vertical column 0
+    //checking horizontal rows
+    for y in 0...2 {
+        var numberOfMarks = 0
+        
+        for x in 0...2{
+            if board[(x,y)] == player{
+                numberOfMarks += 1
+            }
+        }
+        
+        if (numberOfMarks == 3) {
+            return true
+        }
+    }
+    
+
+    let leftToRight: [Coordinate] = [(0,0), (1,1),(2,2)]
+    
     var numberOfMarks = 0
     
-    for y in 0...2{
-        if board[(0,y)] == player{
+    for coordinate in leftToRight {
+        if board[coordinate] == player {
             numberOfMarks += 1
         }
     }
-    if (numberOfMarks == 3) {
+    
+    if (numberOfMarks == 3){
         return true
     }
     
-    //Check another column
+    //reset for the other diagonal
     numberOfMarks = 0
+    let rightToLeft: [Coordinate] = [(2,0), (1,1),(0,2)]
+    for coordinate in rightToLeft {
+           if board[coordinate] == player {
+               numberOfMarks += 1
+           }
+       }
+       if (numberOfMarks == 3){
+           return true
+       }
+    
     
     return false
 }
--- TicTacToeTests/GameAITests.swift
+++ TicTacToeTests/GameAITests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import TicTacToe
 
 class GameAITests: XCTestCase {
-
+    
     func testWinCheckingVertical0() {
         
         var board = GameBoard()
@@ -18,14 +18,49 @@ class GameAITests: XCTestCase {
         try! board.place(mark: .x, on: (0,0))
         try! board.place(mark: .o, on: (1,0))
         
-        try! board.place(mark: .x, on: (0,0))
-        try! board.place(mark: .o, on: (1,0))
+        try! board.place(mark: .x, on: (0,1))
+        try! board.place(mark: .o, on: (1,1))
         
-        try! board.place(mark: .x, on: (0,0))
- 
+        try! board.place(mark: .x, on: (0,2))
+        
         XCTAssertTrue(game(board: board, isWonBy: .x))
         
         XCTAssertFalse(game(board: board, isWonBy: .o))
     }
-
+    
+    func testInProgressCheckingVertical1() {
+        
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (1,0))
+        try! board.place(mark: .o, on: (2,0))
+        
+        try! board.place(mark: .x, on: (1,1))
+        try! board.place(mark: .o, on: (2,1))
+        
+        try! board.place(mark: .x, on: (0,2))
+        
+        XCTAssertFalse(game(board: board, isWonBy: .x))
+        
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+    }
+    
+    
+    func testWinCheckingVertical1() {
+        
+        var board = GameBoard()
+        
+        try! board.place(mark: .x, on: (1,0))
+        try! board.place(mark: .o, on: (2,0))
+        
+        try! board.place(mark: .x, on: (1,1))
+        try! board.place(mark: .o, on: (2,1))
+        
+        try! board.place(mark: .x, on: (1,2))
+        
+        XCTAssertTrue(game(board: board, isWonBy: .x))
+        
+        XCTAssertFalse(game(board: board, isWonBy: .o))
+    }
+    
 }
