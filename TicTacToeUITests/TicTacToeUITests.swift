5c4d0145aceb6af0f5d8ef486c7b74bddc830f89
fritzgt <tuanis@live.com>
2020-04-04 12:48:02 +0300

    created test for label and won situation

--- TicTacToeUITests/TicTacToeUITests.swift
+++ TicTacToeUITests/TicTacToeUITests.swift
@@ -13,11 +13,65 @@ class TicTacToeUITests: XCTestCase {
     private var app:  XCUIApplication {
         return  XCUIApplication()
     }
+    
+    private var turnLabel: XCUIElement {
+        return app.staticTexts["GameViewController.Label"]
+    }
+    
+    
+    override func setUp() {
+        //Stop if a test fails
+        continueAfterFailure = false
+        
+        //Lanch app
+        app.launch()
+    }
 
     func testWinCheckingVertical1()  {
-        app.launch()
-        app.buttons["button0"].tap()
+    
+        let button0 = button(for: 0)
+        let button1 = button(for: 1)
+        let button2 = button(for: 2)
+        let button3 = button(for: 3)
+        let button4 = button(for: 4)
+        let button5 = button(for: 5)
+        let button6 = button(for: 6)
+//        let button7 = button(for: 7)
+//        let button8 = button(for: 8)
         
+        button0.tap()
+        XCTAssertEqual(button0.label, "X")
+        //Check if the label shows the correct turn
+        XCTAssertEqual(turnLabel.label, "Player O's turn")
         
+        button1.tap()
+        XCTAssertEqual(button1.label, "O")
+        XCTAssertEqual(turnLabel.label, "Player X's turn")
+        
+        button2.tap()
+        XCTAssertEqual(button2.label, "X")
+        XCTAssertEqual(turnLabel.label, "Player O's turn")
+        
+        button3.tap()
+        XCTAssertEqual(button3.label, "O")
+        XCTAssertEqual(turnLabel.label, "Player X's turn")
+        
+        button4.tap()
+        XCTAssertEqual(button4.label, "X")
+        XCTAssertEqual(turnLabel.label, "Player O's turn")
+        
+        button5.tap()
+        XCTAssertEqual(button5.label, "O")
+        XCTAssertEqual(turnLabel.label, "Player X's turn")
+        
+        button6.tap()
+        XCTAssertEqual(button6.label, "X")
+        XCTAssertEqual(turnLabel.label, "Player X won!")
+    }
+    
+    
+    //Helper method to create the button extension
+    private func button(for Index: Int) -> XCUIElement {
+        return app.buttons["button\(Index)"]
     }
 }
